fc277b0fec181dd25452297c3821ff623654d252
Galina-S <galyna.sazonova@gmail.com>
2023-02-08 01:02:47 +0100

    fix

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import fs from 'fs';
 import mongoose from "mongoose";
 import multer from "multer";
 import cors from 'cors';
@@ -20,6 +21,9 @@ const app = express();
 
 const storage = multer.diskStorage({
     destination: (_, __, cb) => {
+        if (!fs.existsSync('uploads')) {
+            fs.mkdirSync('uploads')
+        }
         cb(null, 'uploads'); //callback function, save the files in Uploads folder
     },
     filename: (_, file, cb) => {
